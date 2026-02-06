8abbb25a32fc63bc15bf7a8d6ff6d2cefd0292bf
Raphael Vielma <rvielma@gmail.com>
2026-02-05 21:15:02 -0300

    feat: agregar Homebrew formula para argos

---
+++ Formula/argos.rb
@@ -0,0 +1,33 @@
+class Argos < Formula
+  desc "Web Security Scanner for Healthcare Environments"
+  homepage "https://github.com/rvielma/argos"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/rvielma/argos/releases/download/v#{version}/argos-v#{version}-x86_64-apple-darwin.tar.gz"
+      # sha256 will be updated on release
+    end
+
+    on_arm do
+      url "https://github.com/rvielma/argos/releases/download/v#{version}/argos-v#{version}-aarch64-apple-darwin.tar.gz"
+      # sha256 will be updated on release
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/rvielma/argos/releases/download/v#{version}/argos-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      # sha256 will be updated on release
+    end
+  end
+
+  def install
+    bin.install "argos"
+  end
+
+  test do
+    assert_match "templates", shell_output("#{bin}/argos modules")
+  end
+end
