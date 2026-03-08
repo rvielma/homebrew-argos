eac0c78862fd5a5fdbca0ed6ffa294fd359ea88f
Raphael Vielma <rvielma@gmail.com>
2026-03-08 09:57:02 -0300

    chore: actualizar formula v2.3.1

--- Formula/argos.rb
+++ Formula/argos.rb
@@ -1,7 +1,7 @@
 class Argos < Formula
   desc "Web Security Scanner for Healthcare Environments"
   homepage "https://github.com/rvielma/argos"
-  version "2.3.0"
+  version "2.3.1"
   license "MIT"
 
   on_macos do
@@ -12,7 +12,7 @@ class Argos < Formula
 
     on_arm do
       url "https://github.com/rvielma/argos/releases/download/v#{version}/argos-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "a0fc481bf3e77c4399afc3b8ca597a08e9114ba1942f7257903baf3e31e9c4a3"
+      sha256 "859b848a66bb4ffc4680a9368108a4779ba08bbaf524018197c75d03d9c07681"
     end
   end
 
